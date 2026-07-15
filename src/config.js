f4ee3e19ef5687804af83ef12eb55c8b7d19135d
Gi-1612 <gisenicolosi@gmail.com>
2026-07-15 03:19:40 -0300

    Update CSV links in config.js

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@
 // Mientras estén sin completar (con el texto "PEGAR_AQUI..."), la app va a
 // mostrar contenido de ejemplo para que puedas seguir viendo cómo se ve.
 
-export const CSV_DEVOCIONALES = "PEGAR_AQUI_EL_LINK_DEVOCIONALES";
-export const CSV_REUNIONES = "PEGAR_AQUI_EL_LINK_REUNIONES";
-export const CSV_NOVEDADES = "PEGAR_AQUI_EL_LINK_NOVEDADES";
+export const CSV_DEVOCIONALES = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTNJvm8sw8S3_wjbfQXK6vaVGKXZHwKcwZlfVq_DXho7R1-neByIAUIo18RJ7ayCjE6von6yQyjQuZ2/pub?gid=459519071&single=true&output=csv";
+export const CSV_REUNIONES = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTcjFaS67wFG4_GqKhBLLZbV70vJFOFaXpkh9e4AUWnaW5bVNjY-QbgpPXUzWID9tYLvVgu7IhOjb_h/pub?gid=562421421&single=true&output=csv";
+export const CSV_NOVEDADES = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS3k3HWn8h3fL7NdmogX8LHaX177-CyJTz0XLW3fwP6gvRa1tFcAgyd_4oiMTqdWScJ8nG_pAeHgKfc/pub?gid=1492904635&single=true&output=csv";
